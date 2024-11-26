242bd39b4fa6f506f19e3fa58206b2326e784d1e
valeriia_yermakova <lera.yerm08@gmail.com>
2024-11-26 20:30:51 +0200

    fix contacts selectors.js

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,3 +1,6 @@
-export const selectContacts = state => state.contacts.items;
-export const selectIsLoading = state => state.contacts.isLoading;
-export const selectError = state => state.contacts.error;
+export const selectVisibleContacts = (state) => {
+    const filter = state.contacts.filter.toLowerCase();
+    return state.contacts.items.filter((contact) =>
+      contact.name.toLowerCase().includes(filter)
+    );
+  };
